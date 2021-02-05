9df4bbb5d4563ca8284979c138a1928ef6e2ce64
Mitchell <mitchell.thomson@code.berlin>
2021-02-05 18:57:45 +0100

    Routing now automatic
    Even with arrows

--- Assets/Scripts/editMode/lineDraw.cs
+++ Assets/Scripts/editMode/lineDraw.cs
@@ -12,7 +12,8 @@ public class lineDraw : MonoBehaviour
         {
             Gizmos.color=Color.cyan;
             Transform routeToDraw = transform.GetChild(i);
-            if(routeToDraw.tag =="Arrow")
+
+            if(routeToDraw.GetComponent<spot>().isArrow)
             {
                 Gizmos.color=Color.red;
                 Vector3 curArrow = routeToDraw.position;
@@ -21,6 +22,21 @@ public class lineDraw : MonoBehaviour
                 Gizmos.DrawLine(curArrow,leftDraw);
                 Gizmos.color=Color.magenta;
                 Gizmos.DrawLine(curArrow,rightDraw);
+
+                for(int j=0;j<2;j++)
+                {
+                    Gizmos.color=Color.cyan;
+                    Transform arrowPath = routeToDraw.GetChild(j);
+                    
+                    for(int k=0; k<arrowPath.childCount; k++)
+                    {
+                        Transform pathSpot = arrowPath.GetChild(k);
+                        Vector3 cur = pathSpot.position;
+                        Vector3 toDraw = pathSpot.GetComponent<spot>().nextSpot.position;
+
+                        Gizmos.DrawLine(cur,toDraw);
+                    }
+                }
             }
             else
             {
